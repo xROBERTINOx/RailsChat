0d3e96ec58219439b9a4f71b5971d5efdf0746a7
roberto <63375372+xROBERTINOx@users.noreply.github.com>
2024-04-15 18:36:10 +0000

    Current running version: updated database to be connected through supabase(RailsDB)

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,27 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_27_020502) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_graphql"
+  enable_extension "pg_stat_statements"
+  enable_extension "pgcrypto"
+  enable_extension "pgjwt"
+  enable_extension "pgsodium"
+  enable_extension "plpgsql"
+  enable_extension "supabase_vault"
+  enable_extension "uuid-ossp"
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "aal_level", ["aal1", "aal2", "aal3"]
+  create_enum "action", ["INSERT", "UPDATE", "DELETE", "TRUNCATE", "ERROR"]
+  create_enum "code_challenge_method", ["s256", "plain"]
+  create_enum "equality_op", ["eq", "neq", "lt", "lte", "gt", "gte", "in"]
+  create_enum "factor_status", ["unverified", "verified"]
+  create_enum "factor_type", ["totp", "webauthn"]
+  create_enum "key_status", ["default", "valid", "invalid", "expired"]
+  create_enum "key_type", ["aead-ietf", "aead-det", "hmacsha512", "hmacsha256", "auth", "shorthash", "generichash", "kdf", "secretbox", "secretstream", "stream_xchacha20"]
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "string"
@@ -24,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_27_020502) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
